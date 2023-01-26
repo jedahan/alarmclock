90af801da33319eb9cef6a586d9d5a1eadb6e354
Jonathan Dahan <git@jonathan.is>
2023-01-26 17:03:20 -0500

    Allow faking the display in the console

--- blinkenlights.py
+++ blinkenlights.py
@@ -1,17 +1,22 @@
-import board
-import busio
 import sys
 import time
-from adafruit_ht16k33 import matrix
-from adafruit_framebuf import FrameBuffer, MVLSB
 from math import floor
+
+RASPBERRY_PI = False
+
+if RASPBERRY_PI:
+    import board
+    import busio
+    from adafruit_ht16k33 import matrix
+
+from adafruit_framebuf import FrameBuffer, MVLSB
 
 from enum import IntEnum
 
 Color = IntEnum("Color", ["OFF", "GREEN", "RED", "YELLOW"])
 
 
-def makeBuffers(width=32, height=8):
+def makeFramebuffer(width=32, height=8):
     """
     The framebuffer can be thought of as a virtual image, where
     we can map each *bit* to one of the pixels in the display.
@@ -19,37 +24,30 @@ def makeBuffers(width=32, height=8):
     Although we can manipulate the panels directly, its easier
     to build an image, then draw the entire image to the display.
     """
-    width = 32
-    height = 8
     buffer = bytearray(width)  # 1 bytes tall x 8 wide x 4 panels = 32 bytes
-    framebuffer = FrameBuffer(buffer, width, height, MVLSB)
-    return framebuffer, buffer
+    return FrameBuffer(buffer, width, height, MVLSB)
 
 
-def draw(panels, buffer, color=Color.RED):
+def draw(panels, framebuffer, color=Color.RED):
     """
-    note that this takes *the underlying buffer*, not the framebuffer
-
     draw expects the panels to be ordered from left to right
 
-    this is a 'blit' function - it takes the bits in a buffer, and
+    This is a 'blit' function - it takes the bits in a buffer, and
     for each bit that is set to 1, sets the display to RED, ORANGE, or YELLOW
     """
-    width = len(buffer)
-
     for panel in panels:
         panel.fill(Color.OFF)
 
+    width, height = framebuffer.width, framebuffer.height
+
     for x in range(width):
-        bite = buffer[x]
         panel = panels[floor(x / width * len(panels))]
         for y in range(height):
-            bit = 1 << y & bite
-            if bit:
+            if framebuffer.pixel(x, y):
                 panel[width - x, y + 1] = color
 
 
-def numbers(panels: list[any], framebuffer, buffer):
+def numbers(panels: list[any], framebuffer):
     """
     For each panel in panels, write the panel number
 
@@ -58,13 +56,16 @@ def numbers(panels: list[any], framebuffer, buffer):
     If the numbers are out of order, change the order in the addresses array
     """
     framebuffer.fill(0)
-    panel_width = len(framebuffer) / len(panels)
+    panel_width = int(framebuffer.width / len(panels))
 
     for panel, number in enumerate(panels):
-        x = number * panel_width
+        x = (number - 1) * panel_width
         framebuffer.text(f"{number}", x, y=0, color=1)
 
-    draw(panels, buffer, color=random.choice(Color))
+    display(framebuffer)
+
+    if RASPBERRY_PI:
+        draw(panels, framebuffer, color=random.choice(Color))
 
 
 def blinkenlights(panels):
@@ -78,6 +79,19 @@ def blinkenlights(panels):
         panel[x, y] = color
         time.sleep(random.random())
 
+# ascii printer for very small framebuffers!
+def display(framebuffer):
+    print("." * (framebuffer.width + 2))
+    for y in range(framebuffer.height):
+        print(".", end="")
+        for x in range(framebuffer.width):
+            if framebuffer.pixel(x, y):
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print(".")
+    print("." * (framebuffer.width + 2))
+
 
 def run():
     """
@@ -88,21 +102,26 @@ def run():
         python3 blinkenlights.py 0x70 0x74 0x71 0x72
     """
 
-    bus = board.I2C()
-    addresses = bus.scan() if len(sys.argv) < 2 else [ int(address, 16) for address in sys.argv ]
-    info(f"{addresses=}")
+    if RASPBERRY_PI:
+        bus = board.I2C()
+        addresses = bus.scan() if len(sys.argv) < 2 else [ int(address, 16) for address in sys.argv ]
+        info(f"{addresses=}")
 
-    panels = [matrix.Matrix8x8x2(bus) for address in addresses]
+        panels = [matrix.Matrix8x8x2(bus) for address in addresses]
 
-    # Clear the screen and turn the brightness down a bit
-    for panel in panels:
-        panel.fill(Color.OFF)
-        panel.brightness = 0.5
+        # Clear the screen and turn the brightness down a bit
+        for panel in panels:
+            panel.fill(Color.OFF)
+            panel.brightness = 0.5
+
+    else:
+        panels = [ 1, 2, 3, 4]
 
     # TODO: Add signal handling to switch between 'apps'
-    app = random.choice([blinkenlight, numbers])
-    framebuffer, buffer = makeBuffers()
-    app(panels, framebuffer, buffer)
+    # app = random.choice([blinkenlight, numbers])
+    framebuffer = makeFramebuffer()
+    numbers(panels, framebuffer)
+    #app(panels, framebuffer)
 
 
 if __name__ == "__main__":
